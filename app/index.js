3e4fe59a322057f03eba684ebe719b3a01d6557e
Pardeshi-Aditya <mailadityapardeshi@gmail.com>
2024-06-14 00:36:16 +0530

    chore: adding assets and images files.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,42 @@
-import { View, Text } from "react-native";
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from 'expo-router'
+import { COLORS, icons, images, SIZES } from '../constants'
+
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Home = () => {
+
+    const router = useRouter()
+
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+        <SafeAreaView style={{ flex : 1, backgroundColor : COLORS.lightWhite}}>
+            <Stack.Screen
+            options={{
+                headerStyle : { backgroundColor : COLORS.lightWhite, Colors: '#000'},
+                headerShadowVisible : false,
+                headerLeft : () =>{
+                    return <ScreenHeaderBtn iconURL={icons.menu} dimension='60%'/>
+                },
+                headerRight : () =>{
+                    return <ScreenHeaderBtn iconURL={images.profile} dimension='100%'/>
+                },
+                headerTitle : 'header here',
+
+            }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium,
+                }}>
+                    <Welcome/>
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
